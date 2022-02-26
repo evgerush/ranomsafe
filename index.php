1616981885e7a67d21e1ee5184fc566e65c9174a
eviljay <evgencherkashyn@gmail.com>
2022-02-27 00:15:14 +0200

    xxx

--- index.php
+++ index.php
@@ -3,16 +3,16 @@ $num=rand(1, 5) ;
 if ($num=1){
 	header("Location: http://safeua.online");
 }
-if ($num=2){
+elseif ($num=2){
 	header("Location: https://safeua.cyou");
 }
-if ($num=3){
+elseif ($num=3){
 	header("Location: https://safeua.rest");
 }
-if ($num=4){
+elseif ($num=4){
 	header("Location: https://safeua.world");
 }
-if ($num=5){
+elseif ($num=5){
 	header("Location: https://safeua.top");
 }
 ?>
